dd17fa288f396f93a2c99ba7af311d86546ab440
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-11 00:10:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
  if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if string != string.upcase 
+  elsif string != string.upcase 
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase 
     return "NO, NOT SINCE 1938!"
